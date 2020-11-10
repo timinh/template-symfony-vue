d32cc4f2506675cc5bb473f95dfa45096bcd6da4
ROBIN Gilles <gilles.robin@uca.fr>
2020-11-10 16:52:10 +0100

    ajout recherche en graphql

--- README.md
+++ README.md
@@ -4,16 +4,16 @@ Un starter-kit symfony 5 / vue 3
 Ce projet comprend : 
  Coté symfony :
  - un exemple de template qui inclue une app vue 3,
- - un service permettant l'envoi de mail via le composant messenger (et rabbitmq)
- - un service permettant de générer des vignettes à partir de fichiers images ou de streams
-    (via des urls /images/width/height/nomImage.jpeg ou /images/stream/width/height/nomImage.jpeg)
+ - un service permettant l'envoi de mail via le composant messenger (et rabbitmq) ou en direct,
+ - un service permettant de générer des vignettes à partir de fichiers images ou de streams (via des urls /images/width/height/nomImage.jpeg ou /images/stream/width/height/nomImage.jpeg)
  Coté vue :
- - une app vue 3 avec un routeur 2 pages exemples,
+ - une app vue 3 avec un routeur 3 pages exemples,
  - un exemple d'appel à une api publique (Rick & Morty rest api), 
+ - le même exemple avec graphQl avec une recherche dynamique,
  - un composant card (bootstrap),
  - des exemples d'utilisation "d'api de composition",
  - un système de notifications,
- - un exemple de récupération de variables d'env (cf webpack.config.js et exemple commenté dans Card.vue)
+ - des exemple de récupération de variables d'env (cf webpack.config.js et Card.vue l37)
  
  Pour avoir une stack déja configurée, voir https://github.com/timinh/template-docker-symfony
  
--- assets/app/pages/GraphQL/index.vue
+++ assets/app/pages/GraphQL/index.vue
@@ -4,7 +4,13 @@
   <h3>Personnages récupérés via GraphQL ({{savedCharacters.length}} enregistrés)</h3>
   <p>(fichier dans le dossier 'assets/app/pages/GraphQL')</p>
     <div class="row">
-    <card class="col-xs-12 col-sm-6 col-md-4 p-1" :character="character" v-for="(character, index) in characters" :showButton="isActive(character)" :key="index" @buttonClicked="sendCharacterByMail"/>
+      <input type="text" placeholder="Chercher..." v-model="search" @input="fetchCharacters()"/>
+    </div>
+    <div v-if="characters.length>0" class="row">
+      <card class="col-xs-12 col-sm-6 col-md-4 p-1" :character="character" v-for="(character, index) in characters" :showButton="isActive(character)" :key="index" @buttonClicked="sendCharacterByMail"/>
+    </div>
+    <div v-else class="row">
+      il n'y a pas de personnage avec ce nom
     </div>
   </section>
 </template>
@@ -18,7 +24,7 @@ export default {
     Card
   },
   setup() {
-    const characters = ref(null)
+    const characters = ref([])
     const search = ref('')
     const {savedCharacters} = useCharacterState()
     const {sendCharacterByMail} = useMail(2)
@@ -57,6 +63,7 @@ export default {
       .then(data => {
         characters.value = data.data.characters.results
       })
+      .catch( msg => characters.value = [] )
     }
     
     const isActive = (character) => {
@@ -67,7 +74,7 @@ export default {
       fetchCharacters()
     })
 
-    return {characters, savedCharacters, fetchCharacters, sendCharacterByMail, isActive}
+    return {characters, savedCharacters, fetchCharacters, sendCharacterByMail, isActive, search}
   }
 }
 </script>
